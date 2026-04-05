6d20845014bbd6097aacff3b56dad57737a045dd
AlexDeFoc <alex.de.foc@gmail.com>
2026-04-05 12:59:28 +0300

    tested cpu speed

--- Code/App/Core/AppEntryPoint/Main.cpp
+++ Code/App/Core/AppEntryPoint/Main.cpp
@@ -1,9 +1,26 @@
 #include "Main.hpp"
 
+#include <chrono>
+#include <iostream>
+
 int main() {
     App app{};
 
-    app.Start();
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i{0}; i < 100; ++i)
+        app.Start();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    // 5. Print it out
+    std::cout << "------------------------------------" << std::endl;
+    std::cout << "Task creation took: " << duration.count() << " nanoseconds" << std::endl;
+    std::cout << "------------------------------------" << std::endl;
+
+    // 6. CRITICAL: Pause so the window doesn't vanish!
+    std::cout << "Press Enter to close..." << std::endl;
+    std::cin.get();
 
     return 0;
 }
