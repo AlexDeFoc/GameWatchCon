537920a271eaae6b6ac5295408db115f4a0800a3
AlexDeFoc <alex.de.foc@gmail.com>
2026-04-27 21:07:35 +0300

    part3.1fix

--- Code/Sources/Tasks/ToggleAutoSaveStatus.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pch.h"
-#include "AppSettings.h"
-#include "Tasks/ToggleAutoSaveStatus.h"
-#include "Tasks/SettingsMenu.h"
-
-auto gw::tasks::ToggleAutoSaveStatus(gw::Console&, gw::AppState&, gw::AppSettings& app_settings, gw::GameLibrary&) -> void* {
-    app_settings.ToggleAutoSaveStatus();
-    return (void*)gw::tasks::SettingsMenu;
-}
